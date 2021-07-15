2dafc4240178c3e4e27a3d2415d0b7c74da75ea5
Vinay Patel <pate0586@algonquinlive.com>
2021-07-14 20:26:45 -0400

    question 2

---
+++ Assignment-3/src/question2/A.java
@@ -0,0 +1,35 @@
+package question2;
+
+abstract class A {
+	
+	abstract void firstMethod();
+	
+	void secondMethod() 
+	{
+		System.out.println("SECOND");
+		firstMethod();
+	}
+	
+	
+
+}
+
+abstract class B extends A
+{
+	@Override
+	void firstMethod() 
+	{
+		System.out.println("FIRST");
+		thirdMethod();
+	}
+	abstract void thirdMethod();
+}
+
+class C extends B
+{
+	@Override
+	void thirdMethod() {
+		System.out.println("THIRD");
+	}
+}
+
---
+++ Assignment-3/src/question2/MainClass.java
@@ -0,0 +1,12 @@
+package question2;
+
+public class MainClass{
+	
+public static void main(String[] args) {
+C c= new C();
+c.firstMethod();
+c.secondMethod();
+c.thirdMethod();
+
+}
+}
